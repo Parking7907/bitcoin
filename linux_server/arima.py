bc58df768190577513b4be6205c8955053b61cc0
Parking7907 <52812373+Parking7907@users.noreply.github.com>
2021-04-13 14:50:37 +0900

    Create arima.py

---
+++ linux_server/arima.py
@@ -0,0 +1,186 @@
+import numpy as np
+import pandas as pd
+import gc
+import math
+import os.path
+import time
+import matplotlib.pyplot as plt
+from datetime import timedelta, datetime
+from dateutil import parser
+from tqdm import tqdm
+import copy
+from statsmodels.tsa.arima.model import ARIMA
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def df2d_to_array3d(df_2d):
+    # 입력 받은 2차원 데이터 프레임을 3차원 numpy array로 변경하는 함수
+    feature_size = df_2d.iloc[:,2:].shape[1]
+    time_size = len(df_2d.time.value_counts())
+    sample_size = len(df_2d.sample_id.value_counts())
+    array_3d = df_2d.iloc[:,2:].values.reshape([sample_size, time_size, feature_size])
+    return array_3d
+
+
+data_path = './data'
+train_x_df = pd.read_csv(data_path  + "/train_x_df.csv")
+train_y_df = pd.read_csv(data_path  + "/train_y_df.csv")
+test_x_df = pd.read_csv(data_path  + "/test_x_df.csv")
+
+
+
+train_x_array = df2d_to_array3d(train_x_df)
+train_y_array = df2d_to_array3d(train_y_df)
+test_x_array = df2d_to_array3d(test_x_df)
+
+'''
+def plot_series(x_series, y_series):
+    #입력 series와 출력 series를 연속적으로 연결하여 시각적으로 보여주는 코드 입니다.
+    plt.plot(x_series, label = 'input_series')
+    plt.plot(np.arange(len(x_series), len(x_series)+len(y_series)),
+             y_series, label = 'output_series')
+    plt.axhline(1, c = 'red')
+    plt.legend()
+'''
+
+#idx = 1012
+#plot_series(train_x_array[idx,:,1], train_y_array[idx,:,1])
+#plt.show()
+
+idx = 1121
+# train data 중 sample_id 1121에 해당하는 x_series로 arima 모델을 학습한 후
+# y_sereis를 추론
+x_series = train_x_array[idx,:,1]
+y_series = train_y_array[idx,:,1]
+
+# ARIMA의 (p,d,q) 값은 최적화 되지않은 값 입니다.
+model = ARIMA(x_series, order=(3,0,1)) 
+fit  = model.fit()
+preds = fit.predict(1,120, typ='levels')
+
+'''plot_series(x_series, y_series)
+plt.plot(np.arange(1380, 1380+120), preds, label = 'prediction')
+plt.legend()
+plt.show()
+'''
+
+valid_x_df = train_x_df[train_x_df.sample_id < 300]
+valid_y_df = train_y_df[train_y_df.sample_id < 300]
+
+valid_x_array = df2d_to_array3d(valid_x_df)
+valid_y_array = df2d_to_array3d(valid_y_df)
+valid_pred_array = np.zeros([300, 120])
+
+for idx in tqdm(range(valid_x_array.shape[0])):
+    try:
+        try:
+            x_series = valid_x_array[idx,:,1]
+
+            model = ARIMA(x_series, order=(5,1,1))
+            fit  = model.fit()
+            preds = fit.predict(1,120, typ='levels')
+            valid_pred_array[idx,:] = preds# - (preds[0]-x_series[-1])
+        except:
+            print("order 4 1 1")
+            x_series = valid_x_array[idx,:,1]
+
+            model = ARIMA(x_series, order=(4,1,1))
+            fit  = model.fit()
+            preds = fit.predict(1,120, typ='levels')
+            valid_pred_array[idx,:] = preds
+    except:
+        print(idx, " 샘플은 수렴하지 않습니다.")
+        # ARIMA의 (p,d,q) 값이 (5,1,1), (4,1,1)에서 수렴하지 않을 경우
+        # 모두 0으로 채움
+        pass
+
+def array_to_submission(x_array, pred_array):
+    # 입력 x_arrry와 출력 pred_arry를 통해서 
+    # buy_quantitiy와 sell_time을 결정
+    submission = pd.DataFrame(np.zeros([pred_array.shape[0],2], np.int64),
+                columns = ['buy_quantity', 'sell_time'])
+    submission = submission.reset_index()
+    submission.loc[:, 'buy_quantity'] = 0.1
+    
+    buy_price = []
+    for idx, sell_time in enumerate(np.argmax(pred_array, axis = 1)):
+        buy_price.append(pred_array[idx, sell_time])
+    buy_price = np.array(buy_price)
+    # 115% 이상 상승한하고 예측한 sample에 대해서만 100% 매수
+    submission.loc[:, 'buy_quantity'] = (buy_price > 1.15) * 1
+    # 모델이 예측값 중 최대 값에 해당하는 시간에 매도
+    submission['sell_time'] = np.argmax(pred_array, axis = 1)
+    submission.columns = ['sample_id','buy_quantity', 'sell_time']
+    return submission
+
+valid_submission = array_to_submission(valid_x_array, valid_pred_array)
+print(valid_submission)
+valid_submission.buy_quantity.value_counts()
+def df2d_to_answer(df_2d):
+    # valid_y_df로부터
+    # open 가격 정보가 포함된
+    # [샘플 수, 120분] 크기의 
+    # 2차원 array를 반환하는 함수
+    feature_size = df_2d.iloc[:,2:].shape[1]
+    time_size = len(df_2d.time.value_counts())
+    sample_size = len(df_2d.sample_id.value_counts())
+    sample_index = df_2d.sample_id.value_counts().index
+    array_2d = df_2d.open.values.reshape([sample_size, time_size])
+    sample_index = list(sample_index)
+    return array_2d, sample_index
+
+
+def COIN(y_df, submission, df2d_to_answer = df2d_to_answer):
+    # 2차원 데이터프레임에서 open 시점 데이터만 추출하여 array로 복원
+    # sample_id정보를 index에 저장
+    y_array, index = df2d_to_answer(y_df)
+    
+    # index 기준으로 submission을 다시 선택
+    submission = submission.set_index(submission.columns[0])
+    submission = submission.iloc[index, :]    
+    
+    # 초기 투자 비용은 10000 달러
+    total_momey      = 10000 # dolors
+    total_momey_list = []
+    
+    # 가장 처음 sample_id값
+    start_index = submission.index[0]
+    for row_idx in submission.index:
+        sell_time  = submission.loc[row_idx, 'sell_time']
+        buy_price  = y_array[row_idx - start_index, 0]
+        sell_price = y_array[row_idx - start_index, sell_time]
+        buy_quantity = submission.loc[row_idx, 'buy_quantity'] * total_momey
+        residual = total_momey - buy_quantity
+        ratio = sell_price / buy_price
+        total_momey = buy_quantity * ratio * 0.9995 * 0.9995 + residual        
+        total_momey_list.append(total_momey)
+        
+    return total_momey, total_momey_list
+
+total_momey, total_momey_list = COIN(valid_y_df, valid_submission)
+print(total_momey)
+
+test_pred_array = np.zeros([test_x_array.shape[0],
+                           120])
+for idx in tqdm(range(test_x_array.shape[0])):
+    try:
+        try:
+            x_series = test_x_array[idx,:,1]
+
+            model = ARIMA(x_series, order=(5,1,1))
+            fit  = model.fit()
+            preds = fit.predict(1,120, typ='levels')
+            test_pred_array[idx,:] = preds
+        except:
+            x_series = test_x_array[idx,:,1]
+            model = ARIMA(x_series, order=(4,1,1))
+            fit  = model.fit()
+            preds = fit.predict(1,120, typ='levels')
+            test_pred_array[idx,:] = preds
+    except:
+        print(idx, " 샘플은 수렴하지 않습니다.")
+        pass
+
+submission = array_to_submission(test_x_array, test_pred_array)
+submission.to_csv("baseline_submission.csv", index = False)
